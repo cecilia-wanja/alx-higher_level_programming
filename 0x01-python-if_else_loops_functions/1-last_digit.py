f5d84e3e70e01bcd37c50d2b803842a720a8fd34
cecilia-wanja <cclwanja.cw@gmail.com>
2022-06-01 20:24:16 +0300

    print the last digit of the number stored in the variable number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,6 @@ last_number = int(repr(number)[-1])
 if last_number > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last_number))
 elif last_number == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_number)) 
+    print("Last digit of {} is {} and is 0".format(number, last_number))
 elif last_number < 6 and last_number != 0:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_number))
