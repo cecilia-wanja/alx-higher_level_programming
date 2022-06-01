f5631335b9e851a3a0cb3a91fba7ca8fc0f91dac
cecilia-wanja <cclwanja.cw@gmail.com>
2022-06-02 00:19:39 +0300

    function that checks for lowercase character

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        i = ord(i)
-        if i >= 65 and i <= 90:
-            print("{:c}".format(i), end="")
-    print("")
+    for str in str:
+        if (ord(str) >= 97 and ord(str) <= 122):
+            str = chr(ord(str) - 32)
+            print("{:s}".format(str), end="")
+    print()
