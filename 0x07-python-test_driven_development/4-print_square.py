3662998789fd42af4f5fa230d4203ebcf9236d63
cecilia-wanja <cclwanja.cw@gmail.com>
2022-06-29 01:24:43 +0300

    function that prints a square with the character #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module print_square that prints a square
+"""
+
+
+def print_square(size):
+    """
+    Prints a square
+    """
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be a non-negative integer')
+    if size == 0:
+        return
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
