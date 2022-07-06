c82ce797682d767b5cc41c34b934ec8ebe36cee3
cecilia-wanja <cclwanja.cw@gmail.com>
2022-07-06 22:50:05 +0300

    function that returns an object (Python data structure) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that returns an object 
+(Python data structure) represented by a JSON string"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """returns an object(Python data structure)
+    represented by a json string"""
+    data = json.loads(my_str)
+    return data
